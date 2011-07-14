66c20e1857583a21789075643ef72393121e307b
Marcin Michalowski <h13ronim@gmail.com>
2011-07-14 13:16:02 +0200

    Add rvm_gems_reset

---
+++ shell_aliases.d/rvm_gems_reset.sh
@@ -0,0 +1,12 @@
+function rvm_gems_reset() {
+  echo "This will remove your Gemfile.lock and `rvm gemdir`"
+  read -p "Are you sure (yN)? "
+
+  if [ "$REPLY" == "y" ]
+  then
+    rm -rf Gemfile.lock && \
+      rm -rf `rvm gemdir` && \
+      gem install bundler && \
+      bundle install
+  fi
+}
