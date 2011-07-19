db6cf3cbf2099d3b60f9bb261f37ff4fa9a9af98
Marcin Nowicki <pr0d1r2@gmail.com>
2011-07-19 18:37:11 +0200

    Update build_arch to support ppc.

--- shell_aliases.d/build_arch.sh
+++ shell_aliases.d/build_arch.sh
@@ -1,1 +1,1 @@
-alias build_arch="if [ `sysctl -n hw.optional.x86_64` -eq 1 ]; then echo x86_64; else echo i386; fi"
+alias build_arch="if [ `uname -p | grep powerpc | wc -l | cut -b8` -eq 1 ]; then echo ppc; else if [ `sysctl -n hw.optional.x86_64 2>/dev/null` -eq 1 ]; then echo x86_64; else echo i386; fi; fi"
