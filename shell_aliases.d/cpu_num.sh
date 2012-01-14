aa1c94336a0fab9dce93909718f2859fe0b9e47c
Marcin Nowicki <pr0d1r2@gmail.com>
2012-01-14 16:12:46 +0100

    Allow cpu_num to work on Linux

--- shell_aliases.d/cpu_num.sh
+++ shell_aliases.d/cpu_num.sh
@@ -1,1 +1,10 @@
-alias cpu_num="sysctl -n hw.ncpu"
+function cpu_num() {
+  case $UNAME in
+    Linux)
+      cat /proc/cpuinfo | grep "^processor" | wc -l
+      ;;
+    *)
+      sysctl -n hw.ncpu
+      ;;
+  esac
+}
