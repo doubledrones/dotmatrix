8f93738d7fca3ab5dcc7a6896921853a1006c5ca
Marcin Nowicki <pr0d1r2@gmail.com>
2012-07-04 10:24:42 +0200

    Replace old alias with function.

--- shell_aliases.d/postgres_local_start.sh
+++ shell_aliases.d/postgres_local_start.sh
@@ -1,1 +1,9 @@
-alias postgres_local_start="$MACPORTS_ROOT/lib/postgresql91/bin/postgres -D $MACPORTS_ROOT/var/db/postgresql91/defaultdb"
+function postgres_local_start() {
+  if [ -f $MACPORTS_ROOT/lib/postgresql91/bin/postgres ]; then
+    $MACPORTS_ROOT/lib/postgresql91/bin/postgres -D $MACPORTS_ROOT/var/db/postgresql91/defaultdb
+  elif [ -f $MACPORTS_ROOT/lib/postgresql84/bin/postgres ]; then
+    $MACPORTS_ROOT/lib/postgresql84/bin/postgres -D $MACPORTS_ROOT/var/db/postgresql84/defaultdb
+  else
+    echo "ERROR: PostgreSQL not found"
+  fi
+}
